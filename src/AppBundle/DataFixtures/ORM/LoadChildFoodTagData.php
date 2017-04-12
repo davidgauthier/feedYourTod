af6ca64a30ada943fde5fa4092ec1d5b7a24580f
Dave <dtirnan@hotmail.com>
2017-04-12 14:59:31 +0200

    child food tag

---
+++ src/AppBundle/DataFixtures/ORM/LoadChildFoodTagData.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace AppBundle\DataFixtures\ORM;
+
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use AppBundle\Entity\ChildFoodTag;
+
+class LoadChildFoodTypeData extends AbstractFixture implements OrderedFixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        $food = [
+            'carrot',
+            'banana',
+            'shrimp',
+            'nuts',
+            'red beans',
+            'apple',
+        ];
+
+        foreach ($food as $i => $food) {
+
+            $foodtag = new ChildFoodTag();
+            $foodtag->setWording($food);
+            $manager->persist($foodtag);
+            $this->addReference('foodtag-'.$i, $foodtag);
+        }
+        $manager->flush();
+    }
+    
+    public function getOrder()
+    {
+        return 60;
+    }
+}
